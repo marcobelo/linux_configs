4fb9e19eeab468ffabb4cb050815a9ba81a727fa
marcobelo <marco.barone.belo@gmail.com>
2021-01-30 17:06:58 -0300

    fix: questions now is auto answered using default option

--- manjaro.sh
+++ manjaro.sh
@@ -1,11 +1,14 @@
-sudo pacman --noconfirm -S base-devel git go snapd
+sudo pacman --noconfirm -S base-devel go snapd
 sudo systemctl enable --now snapd.socket
 git clone https://aur.archlinux.org/yay.git
-cd yay && makepkg -si && cd .. && rm -rf yay/
-yay -SYg --nocleanmenu --nodiffmenu terminator
-yay -SYg --nocleanmenu --nodiffmenu visual-studio-code-bin
-yay -SYg --nocleanmenu --nodiffmenu google-chrome
-yay -SYg --nocleanmenu --nodiffmenu docker
-yay -SYg --nocleanmenu --nodiffmenu docker-compose
-yay -SYg --nocleanmenu --nodiffmenu nvm
-yay -SYg --nocleanmenu --nodiffmenu jdk
+cd yay && makepkg -si --noconfirm && cd .. && rm -rf yay/
+yay -Syyu --noconfirm \
+    terminator \
+    xclip \
+    visual-studio-code-bin \
+    google-chrome \
+    docker \
+    docker-compose \
+    nvm \
+    jdk \
+    steam-manjaro
