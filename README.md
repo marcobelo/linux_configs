94d94edb83282f984b551b092bdf174d8ded94d9
marcobelo <marco.barone.belo@gmail.com>
2021-02-01 19:37:00 -0300

    feat: add ssh-keygen to config_git.sh

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ Auto config your newly installed manjaro.
 
 # How to use it:
 
-Configure your git user (name and email) at constants.sh, and than run manjaro.sh
+Configure your git user (name and email) at constants.sh, and then run manjaro.sh
--- config_git.sh
+++ config_git.sh
@@ -13,3 +13,7 @@ echo "[core]
     s = !git status -s
     c = !git add --all && git commit -m
     l = !git log --pretty=format:'%C(blue)%h %C(red)%d %C(white)%s - %C(cyan)%cn, %C(green)%cr'" >> ~/.gitconfig
+
+ssh-keygen -t rsa -b 4096 -C "$GIT_USER_EMAIL"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
