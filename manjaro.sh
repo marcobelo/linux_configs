7dccc1385ce70199bcbcfac1e7f2b09d92e51e25
marcobelo <marco.barone.belo@gmail.com>
2021-03-06 23:26:15 -0300

    add timezone config to manjaro.sh and more cool apps

--- manjaro.sh
+++ manjaro.sh
@@ -1,7 +1,13 @@
-sudo pacman --noconfirm -S base-devel go snapd
+sudo pacman --noconfirm -S base-devel go snapd ntp
 sudo systemctl enable --now snapd.socket
+sudo timedatectl set-ntp true
+# Setting timezone to America/Sao_Paulo, change for your timezone
+# Use this command to find out yours: $ timedatectl list-timezones
+sudo timedatectl set-timezone America/Sao_Paulo
+
 git clone https://aur.archlinux.org/yay.git
 cd yay && makepkg -si --noconfirm && cd .. && rm -rf yay/
+
 yay -Syyu --noconfirm \
     terminator \
     xclip \
@@ -11,12 +17,29 @@ yay -Syyu --noconfirm \
     docker-compose \
     nvm \
     jdk \
-    steam-native \
-    steam-manjaro \
+    jre \
     github-cli \
+    github-desktop \
     insomnia \
     discord \
+    xf86-input-wacom \
+    flameshot \
+    minder \
+    dockbarx \
+    jetbrains-toolbox \
     nerd-fonts-complete
 
 echo 'source /usr/share/nvm/init-nvm.sh' >> ~/.zshrc
 exec $SHELL
+
+flatpak install flathub com.github.calo001.fondo
+snap install slack spotify --classic
+
+
+#* Configuring wacom:
+#* list monitor devices
+# $ xrandr --query
+#* list wacom devices
+# $ xsetwacom list devices
+#* set wacom to be confined in one monitor
+# $ xsetwacom set 19 MapToOutput 1920x1080+1280+0
